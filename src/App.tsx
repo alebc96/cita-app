29db011d4fdbf39b0354eb8a7349eda83e616196
alebc96 <alebecerrac96@gmail.com>
2026-04-17 16:10:17 +0200

    pues hay otro mas x3 hehe

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Pregunta />} />
+      <Route path="/cita-app" element={<Pregunta />} />
+      <Route path="/cita-app/exito" element={<YesPage />} />
       <Route path="/exito" element={<YesPage />} />
       <Route path="/fracaso-garrafal" element={<NoPage />} />
+      <Route path="/cita-app/fracaso-garrafal" element={<NoPage />} />
     </Routes>
   );
 }
